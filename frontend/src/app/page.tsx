4b0aa5d32e0e187cd07e3ec525cd5e8d8773e3cb
ty-dotcom <128314291+ty-dotcom@users.noreply.github.com>
2025-12-03 22:34:24 -0500

    home

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,20 @@
-import CreateMeetingPage from "../app/meets/CreateMeetingPage";
 
 export default function Home() {
-  return <CreateMeetingPage/>
+  return (
+    <div className="flex h-screen w-full flex-col">
+      <div className="text-2xl text-white font-bold">
+        Welcome to our Integrated Psych Facility Application
+      </div>
+      <div className="text-white font-bold">
+        Mind, Heart & Soul Counselling is a counselling facility located in Lawton, Oklahoma, United States of America, founded in 2011 and run by a  prominent relationship therapist, clinical director and business owner, Sheila Hodge-Windover. 
+        Within this facility, she handles a broad spectrum of clients, such as domestic violence victims, abused children, and couples in need of counselling be it marital or premarital, to name a few. 
+        Currently, this business has web-based software in place; however, the business owner has noticed the difficulty of using and paying for various software, rather than having a fully integrated software, where patients and the counsellor will have a much better navigation experience.
+        An integrated software will also decrease the need for additional software applications to complete a single task. There also exists, the issue in which despite having said software patients would still need to complete certain processes in-person, such as payments, bookings, and filling out necessary forms, which often leads to an excessive queue at the facility. 
+        The new software system must be able to either reduce or negate this process as a whole, by allowing clients to complete said processes online, as well as integrate existing features in the current system and any additional features to improve its intended users, clients and counsellors, experience. 
+        The system’s capabilities will include: real-time video session platforms and messaging between clients and counsellors, booking sessions utilizing fast online payment, distributing therapy-based resources created by the owner, reports created by clients to track their own progress, as well as reports produced by counselors to track their clients’ progress for comparison, and display this data via data reports to the counselors and finally, secure data collection via forms and displaying this data to the counsellors. 
+        The system will also ensure high levels of safety and security to the client’s sensitive information.
+
+      </div>
+    </div>
+  )
 }
